165f6248351723e309427e2278aeac7f4f280ce8
Monsta <44716687+AthallahDzaki@users.noreply.github.com>
2020-08-18 22:17:14 +0700

    Adding audiostream

--- jni/main.h
+++ jni/main.h
@@ -8,12 +8,26 @@ SGBTEAM lib Thanks to Delvin Hatanaskov santrope-rp to help me about blabla indi
 
 #pragma once
 
+#include <jni.h>
+#include <android/log.h>
+#include <ucontext.h>
+#include <pthread.h>
+#include <malloc.h>
 #include <cstdlib>
+#include <stdlib.h>
 #include <string>
+#include <string.h>
+#include <sstream>
+#include <iostream>
 #include <vector>
 #include <list>
+#include <cmath>
+#include <thread>
+#include <chrono>
+#include <cstdarg>
+#include <sys/mman.h>
 #include <unistd.h>
-#include <stdint.h>
+#include <unordered_map>
 
 #define DEBUG 1
 
@@ -40,6 +54,21 @@ extern uintptr_t g_libGTASA;
 extern const char* g_pszStorage;
 extern const short iNetGameCookie;
 extern const short iNetGameVersion;
+
+// AUDIO STREAM
+extern int (*BASS_Init)(uint32_t, uint32_t, uint32_t);
+extern int (*BASS_Free)(void);
+extern int (*BASS_SetConfigPtr)(uint32_t, const char*);
+extern int (*BASS_SetConfig)(uint32_t, uint32_t);
+extern int (*BASS_ChannelStop)(uint32_t);
+extern int (*BASS_StreamCreateURL)(char*, uint32_t, uint32_t, uint32_t);
+extern int (*BASS_ChannelPlay)(uint32_t);
+extern int *BASS_ChannelGetTags;
+extern int *BASS_ChannelSetSync;
+extern int *BASS_StreamGetFilePosition;
+extern int (*BASS_StreamFree)(uint32_t);
+
+void LoadBassLibrary();
 
 // Quit Game / Exit Game
 void QuitGame();
